5e305afd19af2116fd75cc1ab9f3895723501341
beanboi-svg <xdbeanboi@gmail.com>
2026-02-15 21:40:48 -0700

    Updated trigger for animation and song to flowers instead of hero-text

--- logic.js
+++ logic.js
@@ -1,8 +1,5 @@
-const heroText = document.querySelector('.hero-text');
 const flowerEls = document.querySelectorAll('.flower-one, .flower-two');
 const loveAudio = new Audio('assets/love.mp3');
-const tapsNeeded = 3;
-let tapCount = 0;
 
 function startFlowerSpin() {
     flowerEls.forEach((flower) => flower.classList.add('is-spinning'));
@@ -14,17 +11,12 @@ function stopFlowerSpin() {
 
 loveAudio.addEventListener('ended', stopFlowerSpin);
 
-if (heroText) {
-    heroText.addEventListener('pointerup', () => {
-        tapCount += 1;
-
-        if (tapCount >= tapsNeeded) {
-            loveAudio.currentTime = 0;
-            startFlowerSpin();
-            loveAudio.play().catch(() => {
-                stopFlowerSpin();
-            });
-            tapCount = 0;
-        }
+flowerEls.forEach((flower) => {
+    flower.addEventListener('pointerup', () => {
+        loveAudio.currentTime = 0;
+        startFlowerSpin();
+        loveAudio.play().catch(() => {
+            stopFlowerSpin();
+        });
     });
-}
+});
