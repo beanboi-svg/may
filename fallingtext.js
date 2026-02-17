83630a932b37aeab3da546df98d3e4e37f2d42cd
beanboi-svg <xdbeanboi@gmail.com>
2026-02-16 18:46:07 -0700

    Fix syntax error in fallingtext.js

--- fallingtext.js
+++ fallingtext.js
@@ -8,8 +8,7 @@ const tempTrackPaths = [
 ];
 const callFallingLines = [
     'Can I have your number?',
-    'Can I shake my leg while I read?',
-    '"Hot."'
+    'Can I shake my leg while I read?'
 ];
 
 let tempCurrentAudio = null;
